b3fab2fbe7c52c7050581f1873aa74ae9decf67f
Devon Poston <devonposton1@gmail.com>
2023-06-08 14:45:58 -0400

    Added update-book and create-book page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var router = express.Router();
 var Book = require('../models').Book
 const path = require('path');
 var app = express();
@@ -9,33 +8,44 @@ var app = express();
   app.set('views', './views');
   app.set('view engine', 'pug');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  const books = await Book.findAll();
-  console.log(books);
-  res.json(books);
+
+
+/*Home Page*/
+app.get('/', async function(req, res, next) {
+  //const books = await Book.findAll();
+  //console.log(books);
+  //res.json(books);
+  res.redirect('/books');
 });
 
-router.get('/books', async function(req, res, next) {
+app.get('/books', async function(req, res, next) {
   const books = await Book.findAll();
   res.render('index', { title: 'Book List', books });
 });
 
-router.get('/books/new', async function(req, res, next) {
+
+
+
+/* New Book Page*/
+app.get('/books/new', async function(req, res, next) {
   res.render('new-book', { title: 'New Book' });
 });
 
-router.post('/books/new', async function(req, res, next) {
+app.post('/books/new', async function(req, res, next) {
   const { title, author, genre, year } = req.body;
   const book = await Book.create({ title, author, genre, year });
   res.redirect('/books/' + book.id);
 });
 
-router.get('/books/:id', async function(req, res, next) {
+
+
+
+/* Update-Book Page*/
+app.get('/books/:id', async function(req, res, next) {
   const bookId = req.params.id;
   const book = await Book.findByPk(bookId);
   if (book) {
-    res.render('book-detail', { title: book.title, book });
+    res.render('update-book', { title: book.title, book });
   } else {
     const error = new Error('Book not found');
     error.status = 404;
@@ -43,20 +53,17 @@ router.get('/books/:id', async function(req, res, next) {
   }
 });
 
-router.post('/books/:id', async function(req, res, next) {
+app.post('/books/:id', async function(req, res, next) {
+  const { title, author, genre, year } = req.body;
     const book = await Book.findByPk(req.params.id);
-    if (!book) {
-      return res.status(404).send('Book not found');
-    }
-    try {
-      await book.update(req.body);
-      res.send('Book updated successfully');
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  });
+    await book.update({ title, author, genre, year });
+    res.redirect(`/books/`);
+});
 
-router.post('/books/:id/delete', async function(req, res, next) {
+
+
+/* Update-book Page delete button*/
+app.post('/books/:id/delete', async function(req, res, next) {
   const book = await Book.findByPk(req.params.id);
   if (!book) {
     return res.status(404).send('Book not found');
@@ -69,22 +76,26 @@ router.post('/books/:id/delete', async function(req, res, next) {
   }
 });
 
+
+
 // Sets up middleware to catch undefined routes
-router.use((req, res, next) => {
+app.use((req, res, next) => {
   const error = new Error('Sorry, the page you requested could not be found.');
   error.status = 404;
   next(error);
 });
 
 // Sets up error handling middleware
-router.use((error, req, res, next) => {
+app.use((error, req, res, next) => {
   res.status(error.status || 500);
   if (error.status === 404) {
   res.send('Sorry! It appears the page you requested could not be found.');
-  console.log('Sorry! It appears the page you requested could not be found.');
   } else {
     res.json({ error: error.message });
   }
 });
 
-module.exports = router;
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
